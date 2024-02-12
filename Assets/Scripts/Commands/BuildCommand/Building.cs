673125722738470a714acd3028bb00cd6a76ae28
Noyan Berkay Aşkın <74705281+Vitoleone@users.noreply.github.com>
2024-02-12 11:00:37 +0300

    Selecting indicator added

--- Assets/Scripts/Commands/BuildCommand/Building.cs
+++ Assets/Scripts/Commands/BuildCommand/Building.cs
@@ -6,6 +6,7 @@ using UnityEngine.EventSystems;
 
 public class Building : MonoBehaviour
 {
+    [SerializeField] GameObject selectedCircle;
     private void Start()
     {
         SelectManager.instance.allUnits.Add(gameObject);
@@ -13,10 +14,14 @@ public class Building : MonoBehaviour
     private void OnMouseDown()
     {
         if (!SelectManager.instance.selectedUnits.Contains(gameObject))
+        {
             SelectManager.instance.SelectUnit(gameObject);
+            selectedCircle.SetActive(true);
+        }
         else
         {
             SelectManager.instance.DeSelectUnit(gameObject);
+            selectedCircle.SetActive(false);
         }
     }
 
@@ -25,6 +30,7 @@ public class Building : MonoBehaviour
         if (SelectManager.instance.selectedUnits.Contains(gameObject))
         {
             SelectManager.instance.DeSelectUnit(gameObject);
+            selectedCircle.SetActive(false);
         }
     }
 
@@ -33,6 +39,7 @@ public class Building : MonoBehaviour
         if (SelectManager.instance.selectedUnits.Contains(gameObject))
         {
             SelectManager.instance.DeSelectUnit(gameObject);
+            selectedCircle.SetActive(false);
         }
     }
 
